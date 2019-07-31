27a5693d4523c3abd1e3f4df34f164d616745d34
Florian Schulz <adrian.endrich@maibornwolff.de>
2019-07-31 18:22:08 +0200

    adds simple dummy debug view

--- app/src/main/java/com/mrpatpat/sensedash/DebugActivity.kt
+++ app/src/main/java/com/mrpatpat/sensedash/DebugActivity.kt
@@ -3,7 +3,7 @@ package com.mrpatpat.sensedash
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class DebugActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
